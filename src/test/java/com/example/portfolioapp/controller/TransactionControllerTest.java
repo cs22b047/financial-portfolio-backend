14f8483e3b5d940cbc7646195911b89cbb3be6ce
lucyfier56 <rudrapandarp.73@gmail.com>
2026-02-04 16:38:39 +0530

    Fixing applicationContext loading failures

--- src/test/java/com/example/portfolioapp/controller/TransactionControllerTest.java
+++ src/test/java/com/example/portfolioapp/controller/TransactionControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TransactionControllerTest {
 
     private MockMvc mockMvc;
