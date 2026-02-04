e874eafa8c36a57de76a92ec6e1d40e64cfedeba
cs22b047 <cs22b047@iittp.ac.in>
2026-02-04 11:02:45 +0530

    fix: enabled cross origin requests in TransactionController

--- src/main/java/com/example/portfolioapp/controller/TransactionController.java
+++ src/main/java/com/example/portfolioapp/controller/TransactionController.java
@@ -17,6 +17,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/transactions")
+@CrossOrigin(origins = "http://localhost:8080")
 public class TransactionController {
 
     @Autowired
